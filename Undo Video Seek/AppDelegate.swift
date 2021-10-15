89358fd8e8bd64caafe439868df6f301d4c3133f
Matthew Murphy <matt@mattmurphy.ca>
2021-10-14 21:00:02 -0600

    Initial Commit

---
+++ Undo Video Seek/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Undo Video Seek
+//
+//  Created by Matthew Murphy on 2021-10-14.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
