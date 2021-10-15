8bca3deddb7ef8ff68b8d33af9dc01fd03eb6224
Matthew Murphy <matt@mattmurphy.ca>
2021-10-14 23:26:09 -0600

    add detector of video data load to clear skipHistory when a new video is clicked (or comes up on autoplay)

--- Undo Video Seek Extension/Resources/content.js
+++ Undo Video Seek Extension/Resources/content.js
@@ -6,12 +6,19 @@ browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
 		
 		// track last updated time to detect a skip rather than second-second playback
 		let prevTime = Math.ceil(video.currentTime)
+		
+		// detect change of video source
+		video.onloadeddata = e => {
+			skipHistory = []
+			prevTime = Math.ceil(video.currentTime)
+			console.log('Data load detected. Skip history cleared.')
+		}
 
 		video.ontimeupdate = e => {
 				const time = Math.ceil(video.currentTime)
 				if (Math.abs(time - prevTime) > 1) { // when position moved more than one second in either direction
 					skipHistory.push(prevTime)
-					console.log(`Skip recorded. Skip history: ${skipHistory}`)
+					console.log('Skip recorded. Skip history:', skipHistory)
 				}
 				prevTime = time
 		}
@@ -23,7 +30,7 @@ browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
 					video.currentTime = prevSkipTime // undo skip
 					prevTime = Math.ceil(video.currentTime) // so undone time isn't detected as another skip
 					skipHistory.pop() // remove undone skip from history
-					console.log(`Skip undone. Skip history: ${skipHistory}`)
+					console.log('Skip undone. Skip history:', skipHistory)
 				}
 			}
 		}
