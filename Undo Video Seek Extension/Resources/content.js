afb0c57713719f10af37fe6fb89a256d86ea7e8f
Matthew Murphy <matt@mattmurphy.ca>
2021-10-14 23:10:17 -0600

    implement functionality

--- Undo Video Seek Extension/Resources/content.js
+++ Undo Video Seek Extension/Resources/content.js
@@ -1,7 +1,33 @@
 browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
+	const video = document.getElementsByTagName('video')[0]
+
+	if (video) {
+		let skipHistory = []
+		
+		// track last updated time to detect a skip rather than second-second playback
+		let prevTime = Math.ceil(video.currentTime)
+
+		video.ontimeupdate = e => {
+				const time = Math.ceil(video.currentTime)
+				if (Math.abs(time - prevTime) > 1) { // when position moved more than one second in either direction
+					skipHistory.push(prevTime)
+					console.log(`Skip recorded. Skip history: ${skipHistory}`)
+				}
+				prevTime = time
+		}
+		
+		document.onkeypress = e => {
+			if (e.key === 'u' && e.metaKey) { // metaKey = command key
+				if (skipHistory.length > 0) { // only try to undo as far as possible
+					const prevSkipTime = skipHistory[skipHistory.length - 1]
+					video.currentTime = prevSkipTime // undo skip
+					prevTime = Math.ceil(video.currentTime) // so undone time isn't detected as another skip
+					skipHistory.pop() // remove undone skip from history
+					console.log(`Skip undone. Skip history: ${skipHistory}`)
+				}
+			}
+		}
+	}
 });
 
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
-});
+browser.runtime.onMessage.addListener((request, sender, sendResponse) => {});
